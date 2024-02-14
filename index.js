f8075c1f56e89175c904c3275b9a35fce8bd03a8
Mike Yu <michaelyyu12@gmail.com>
2024-02-14 09:50:31 -0500

    new changes

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to My Page</h1>
+        <p>This is the content of the page.</p>
+      </div>
+    );
+  }
+}
+
+export default Page;
---
+++ module.js
@@ -0,0 +1,7 @@
+export const greeting = "Hello, world!";
+
+// Exporting a function
+export function sayHello() {
+  console.log("Hello!");
+}
+
